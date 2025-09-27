f484135b79a94d8df6ac8d69c7eb8165df978027
utyara3 <prorok2018pro@gmail.com>
2025-09-27 14:58:37 +0300

    complete database

--- databse/base.py
+++ databse/base.py
@@ -1,9 +1,10 @@
+from sqlite3 import DatabaseError
 import aiosqlite
 
 from config import DATABASE_PATH
 
 
-async def init_db():
+async def init_db() -> None:
     async with aiosqlite.connect(DATABASE_PATH) as conn:
         async with conn:
             await conn.execute('PRAGMA foreign_keys = ON')
@@ -54,12 +55,12 @@ async def init_db():
 
 async def is_user_in_database(user_tg_id: int) -> bool:
     async with aiosqlite.connect(DATABASE_PATH) as conn:
-        async with conn.execute(f'SELECT 1 FROM users WHERE th_id=?', (user_tg_id, )) as cursor:
-            return cursor.fetchone() is not None
+        async with conn.execute(f'SELECT 1 FROM users WHERE tg_id=?', (user_tg_id, )) as cursor:
+            return await cursor.fetchone() is not None
         
 
 async def insert_user_to_database(tg_id: int, username: str = '', first_name: str = '', 
-                                  last_name: str = '', roles: tuple['str'] = ('student', )) -> None:
+                                  last_name: str = '', roles: tuple[str] = ('student', )) -> None:
     async with aiosqlite.connect(DATABASE_PATH) as conn:
         async with conn:
             await conn.execute('INSERT OR IGNORE INTO users (tg_id, username, first_name, last_name) VALUES (?, ?, ?, ?)', 
@@ -77,5 +78,70 @@ async def insert_user_to_database(tg_id: int, username: str = '', first_name: st
 
                 role_ids = [row[0] for row in await cursor.fetchall()] 
 
-                for role_id in role_ids:
-                    await conn.execute('INSERT OR IGNORE INTO user_roles(user_id, role_id) VALUES (?, ?)', (user_id, role_id))
+                if role_ids:
+                    role_data = [(user_id, role_id) for role_id in role_ids]
+                    await conn.executemany('INSERT OR IGNORE INTO user_roles(user_id, role_id) VALUES (?, ?)', role_data)
+
+
+async def get_user_roles(tg_id: int) -> list[str]:
+    async with aiosqlite.connect(DATABASE_PATH) as conn:
+        cursor = await conn.execute("""
+            SELECT r.name 
+            FROM users u
+            JOIN user_roles ur
+                ON ur.user_id = u.id
+            JOIN roles r
+                ON ur.role_id = r.id
+            WHERE u.tg_id = ?
+        """, (tg_id, ))
+
+        roles = await cursor.fetchall()
+
+        return [role[0] for role in roles]
+    
+
+async def has_user_role(tg_id: int, role: str) -> bool:
+    return role in await get_user_roles(tg_id)
+    
+
+async def add_role_to_user(tg_id: int, role_name: str) -> None:
+    async with aiosqlite.connect(DATABASE_PATH) as conn:
+        async with conn:
+            cursor = await conn.execute("SELECT id FROM users WHERE tg_id = ?", (tg_id, ))
+            user = await cursor.fetchone()
+
+            cursor = await conn.execute("SELECT id FROM roles WHERE name = ?", (role_name, ))
+            role = await cursor.fetchone()
+
+            if user and role:
+                user_id = user[0]
+                role_id = role[0]
+
+                await conn.execute("""INSERT OR IGNORE INTO user_roles (user_id, role_id) VALUES (?, ?)""", (user_id, role_id))
+
+
+async def remove_role_for_user(tg_id: int, role_name: str) -> None:
+    async with aiosqlite.connect(DATABASE_PATH) as conn:
+        async with conn:
+            await conn.execute("""
+                DELETE FROM user_roles
+                WHERE user_id = (SELECT id FROM users WHERE tg_id = ?)
+                AND role_id = (SELECT id FROM roles WHERE name = ?)
+            """, (tg_id, role_name, ))
+
+
+async def get_users_by_role(role_name: str) -> list[int]:
+    async with aiosqlite.connect(DATABASE_PATH) as conn:
+        cursor = await conn.execute("""
+            SELECT u.tg_id
+            FROM users u
+            JOIN user_roles ur
+                ON u.id = ur.user_id
+            JOIN roles r
+                ON ur.role_id = r.id
+            WHERE r.name = ?
+        """, (role_name, ))
+
+        users = await cursor.fetchall()
+
+        return [user[0] for user in users]
